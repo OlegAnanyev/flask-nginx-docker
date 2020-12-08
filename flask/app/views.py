4b5658f95ed521ab0ae020a6f7dea37cb3568609
Oleg Ananyev <32748936+OlegAnanyev@users.noreply.github.com>
2020-12-08 11:16:10 +0300

    Update views.py

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,13 +1,14 @@
 from app import app
 import os
+from datetime import datetime, date, time
 
 @app.route("/")
 def index():
 
     # Use os.getenv("key") to get environment variables
     app_name = os.getenv("APP_NAME")
-
+    date_time = datetime.today()
     if app_name:
-        return f"Hello from {app_name} running in a Docker container behind Nginx!</br><img src='https://ananyev.me/images/profile-img.jpg'>"
+        return f"Hello from {app_name} running in a Docker container behind Nginx!</br>{date_time}</br><img src='https://ananyev.me/images/profile-img.jpg'>"
 
     return "Hello from Flask"
